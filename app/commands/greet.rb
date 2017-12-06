f7949bcbb3daae904de089cb8515e15c049fece0
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-06 17:35:10 +0000

    initial application with buttons

--- app/commands/greet.rb
+++ app/commands/greet.rb
@@ -1,18 +1,48 @@
 module Ombuddy
 	module Commands
-		INTRODUCTION_TEXT = [
-			"Hello! I'm Ombuddy, and I'm here to help. Just let me know what you need with any of the following commands:",
-			"\n*guidelines*",
-			"to see your company's HR guidelines and learn about your reporting options",
-			"\n*report*",
-			"to file a report about an HR issue",
-			"\n*help*",
-			"for more information about Ombuddy or alternative support options"
-			].join("\n")
+		INTRODUCTION_TEXT = "Hello! I'm Ombuddy. I'm here to help you manage and resolve workplace issues. How can I help today?"
+
+		ATTACHMENTS = [
+        {
+        	"text": "I want to...",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+            	{
+            		"name": "action",
+            		"text": "Create a report",
+            		"style": "danger",
+            		"type": "button",
+            		"value": "report",
+            		"confirm": {
+            			"title": "Are you sure?",
+            			"text": "Wouldn't you prefer a good game of chess?",
+            			"ok_text": "Yes",
+            			"dismiss_text": "No"
+            		}
+            	},
+            	{
+            		"name": "action",
+            		"text": "Get some advice",
+            		"type": "button",
+                    "value": "guidelines"
+                },
+                {
+                    "name": "action",
+                    "text": "Find out more about Ombuddy",
+                    "type": "button",
+                    "value": "information"
+                },
+            ]
+        }
+    ]
 
 		class Greet < SlackRubyBot::Commands::Base
 			command 'hello', 'hi' do |client, data, _match|
 				client.say(channel: data.channel, text: INTRODUCTION_TEXT)
+				client.web_client.chat_postMessage(channel: data.channel, attachments: ATTACHMENTS)
 			end
 		end
 	end
