6be4f4e612df6e5216ab038c348b63eb7d3c8ac6
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-06 12:58:37 +0000

    Change token given that repository is now private

--- app/commands/greet.rb
+++ app/commands/greet.rb
@@ -1,17 +1,17 @@
 module Ombuddy
 	module Commands
 		INTRODUCTION_TEXT = [
-			"Hello! I'm OmBuddy, and I'm here to help. Just let me know what you need with any of the following commands:",
+			"Hello! I'm Ombuddy, and I'm here to help. Just let me know what you need with any of the following commands:",
 			"\n*guidelines*",
 			"to see your company's HR guidelines and learn about your reporting options",
 			"\n*report*",
 			"to file a report about an HR issue",
 			"\n*help*",
-			"for more information about OmBuddy or alternative support options"
+			"for more information about Ombuddy or alternative support options"
 			].join("\n")
 
 		class Greet < SlackRubyBot::Commands::Base
-			command 'hello' do |client, data, _match|
+			command 'hello', 'hi' do |client, data, _match|
 				client.say(channel: data.channel, text: INTRODUCTION_TEXT)
 			end
 		end
