3516db7078ae1f7f8998c28c14a6f5b1f51e3120
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-06 18:42:37 +0000

    Remove unnecessary options

--- app/commands/greet.rb
+++ app/commands/greet.rb
@@ -3,40 +3,32 @@ module Ombuddy
 		INTRODUCTION_TEXT = "Hello! I'm Ombuddy. I'm here to help address your HR concerns. How can I help today?"
 
 		ATTACHMENTS = [
-        {
-        	"text": "I want to...",
-            "fallback": "You are unable to choose a game",
-            "callback_id": "wopr_game",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-            	{
-            		"name": "action",
-            		"text": "Create a report",
-            		"style": "danger",
-            		"type": "button",
-            		"value": "report",
-            		"confirm": {
-            			"title": "Are you sure?",
-            			"text": "Wouldn't you prefer a good game of chess?",
-            			"ok_text": "Yes",
-            			"dismiss_text": "No"
-            		}
-            	},
-            	{
-            		"name": "action",
-            		"text": "Get some advice",
-            		"type": "button",
-                    "value": "guidelines"
-                },
-                {
-                    "name": "action",
-                    "text": "Find out more about Ombuddy",
-                    "type": "button",
-                    "value": "information"
-                },
-            ]
-        }
+      {
+        "text": "I want to...",
+        "fallback": "You are unable to select an option",
+        "callback_id": "action_selection",
+        "attachment_type": "default",
+        "actions": [
+          {
+            "name": "action",
+            "text": "Create a report",
+            "type": "button",
+            "value": "report",
+          },
+          {
+            "name": "action",
+            "text": "Get some advice",
+            "type": "button",
+            "value": "guidelines"
+          },
+          {
+            "name": "action",
+            "text": "Find out more about Ombuddy",
+            "type": "button",
+            "value": "information"
+          },
+        ]
+      }
     ]
 
 		class Greet < SlackRubyBot::Commands::Base
