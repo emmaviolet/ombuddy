330baf96b1df681bb44bb9c82109edf6803f9c60
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-07 11:00:45 +0000

    adds initial response

--- app/services/start_reporting_process.rb
+++ app/services/start_reporting_process.rb
@@ -3,7 +3,8 @@ module Ombuddy
     class StartReportingProcess < SlackRubyBot::Commands::Base
       
     	def self.call(params)
-
+    		channel = params["channel"]["id"]
+    		client.say(channel: data.channel, text: 'okay! let us do this')
     	end
     end
   end
