cc976cc6bd40032284cead57fbf7344beca0cafc
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-08 15:28:52 +0000

    Add some follow-up questions

--- app/services/select_action.rb
+++ app/services/select_action.rb
@@ -16,19 +16,67 @@ module Ombuddy
 			private
 			def self.attachments_for(button_value)
 				if button_value == 'report'
-					text = ":writing_hand: You asked to create a report"
+					title = ":writing_hand: You asked to create a report."
+					text = "You'll need to speak to the HR team to make a formal report. Is it okay if I pass your details onto them?"
+					actions = [
+          {
+            "name": "report_response",
+            "text": "Yes",
+            "type": "button",
+            "value": "yes",
+          },
+          {
+            "name": "report_response",
+            "text": "No",
+            "type": "button",
+            "value": "no"
+          }
+        ]
 				elsif button_value == 'guidance'
-					text = ":speaking_head_in_silhouette: You asked for some advice"
+					title = ":speaking_head_in_silhouette: You asked for some advice."
+					text = "I know about your HR policy and all the support options available. Is any of that helpful?"
+					actions = [
+          {
+            "name": "guidance_response",
+            "text": "Yes",
+            "type": "button",
+            "value": "yes",
+          },
+          {
+            "name": "guidance_response",
+            "text": "No",
+            "type": "button",
+            "value": "no"
+          }
+        ]
 				else
-					text = "Hmmm, that doesn't seem to be something I know how to do. Care to give me some feedback?"
+					title = "Hmmm, that doesn't seem to be something I know how to do."
+					text = "Care to give me some feedback?"
+					actions = [
+          {
+            "name": "unknown_response",
+            "text": "Yes",
+            "type": "button",
+            "value": "yes",
+          },
+          {
+            "name": "unknown_response",
+            "text": "No",
+            "type": "button",
+            "value": "no"
+          }
+        ]
 				end
 
-				[{
-        	"attachment_type": "default",
-        	"text": text
-      	}]
+				[
+					{
+        		"attachment_type": "default",
+        		"title": title,
+        		"text": text,
+        		"actions": actions
+      		}
+      	]
 			end
-
 
 			def self.message_for(button_value)
 				if button_value == 'report'
