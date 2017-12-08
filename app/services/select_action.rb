ecdf56981b6c67004e3faa1ffc0d02ddb7aecb4e
Emma Makinson <emmavioletmakinson@gmail.com>
2017-12-08 15:55:55 +0000

    Adds select

--- app/services/select_action.rb
+++ app/services/select_action.rb
@@ -17,55 +17,64 @@ module Ombuddy
 			def self.attachments_for(button_value)
 				if button_value == 'report'
 					title = ":writing_hand: You asked to create a report."
-					text = "You'll need to speak to the HR team to make a formal report. Is it okay if I pass your details onto them?"
+					text = "You'll need to speak to the HR team to make a formal report. Can I pass your details onto them?"
 					actions = [
-          {
-            "name": "report_response",
-            "text": "Yes",
-            "type": "button",
-            "value": "yes",
-          },
-          {
-            "name": "report_response",
-            "text": "No",
-            "type": "button",
-            "value": "no"
-          }
-        ]
+					{
+						"name": "report_response",
+						"text": "Yes",
+						"type": "button",
+						"value": "yes",
+					},
+					{
+						"name": "report_response",
+						"text": "No",
+						"type": "button",
+						"value": "no"
+					}
+				]
 				elsif button_value == 'guidance'
 					title = ":speaking_head_in_silhouette: You asked for some advice."
 					text = "I know about your HR policy and all the support options available. Is any of that helpful?"
 					actions = [
-          {
-            "name": "guidance_response",
-            "text": "Yes",
-            "type": "button",
-            "value": "yes",
-          },
-          {
-            "name": "guidance_response",
-            "text": "No",
-            "type": "button",
-            "value": "no"
-          }
-        ]
+						{
+							"name": "guidance_response",
+							"text": "Yes",
+							"type": "select",
+							"options": [
+                        {
+                            "text": "Our policies",
+                            "value": "policies"
+                        },
+                        {
+                            "text": "Support options",
+                            "value": "support"
+                        }
+                      ]
+						},
+						{
+							"name": "guidance_response",
+							"text": "No",
+							"type": "button",
+							"value": "no"
+						}
+					]
 				else
 					title = "Hmmm, that doesn't seem to be something I know how to do."
 					text = "Care to give me some feedback?"
 					actions = [
-          {
-            "name": "unknown_response",
-            "text": "Yes",
-            "type": "button",
-            "value": "yes",
-          },
-          {
-            "name": "unknown_response",
-            "text": "No",
-            "type": "button",
-            "value": "no"
-          }
-        ]
+						{
+							"name": "unknown_response",
+							"text": "Yes",
+							"type": "button",
+							"value": "yes",
+						},
+						{
+							"name": "unknown_response",
+							"text": "No",
+							"type": "button",
+							"value": "no"
+						}
+					]
 				end
 
 				[
@@ -76,16 +85,6 @@ module Ombuddy
         		"actions": actions
       		}
       	]
-			end
-
-			def self.message_for(button_value)
-				if button_value == 'report'
-					return "Okay, I can help you with that. I've created a report for you..."
-				elsif button_value == 'guidance'
-					return "Sure! What would you like to know?"
-				else
-					return "Hmmm, that doesn't seem to be something I know how to do. Care to give me some feedback?"
-				end
 			end
 		end
 	end
