76c1c5fd80e0924799f4cff699f4a6e160eedb89
Grey Baker <greysteil@gmail.com>
2017-12-09 11:10:34 +0000

    Clean up SelectAction service

--- app/services/select_action.rb
+++ app/services/select_action.rb
@@ -20,76 +20,98 @@ module Ombuddy
         private
 
         def attachments_for(button_value)
-          if button_value == "report"
-            title = ":writing_hand: You asked to create a report."
-            text = "You'll need to speak to the HR team to make a formal "\
-                   "report. Can I pass your details onto them?"
-            actions = [
-              {
-                "name": "report_response",
-                "text": "Yes",
-                "type": "button",
-                "value": "yes"
-              },
-              {
-                "name": "report_response",
-                "text": "No",
-                "type": "button",
-                "value": "no"
-              }
-            ]
-          elsif button_value == "guidance"
-            title = ":speaking_head_in_silhouette: You asked for some advice."
-            text = "I know about your HR policy and all the support options "\
-                   "available. Is any of that helpful?"
-            actions = [
-              {
-                "name": "guidance_response",
-                "text": "Yes",
-                "type": "select",
-                "options": [
-                  {
-                    "text": "Our policies",
-                    "value": "policies"
-                  },
-                  {
-                    "text": "Support options",
-                    "value": "support"
-                  }
-                ]
-              },
-              {
-                "name": "guidance_response",
-                "text": "No",
-                "type": "button",
-                "value": "no"
-              }
-            ]
-          else
-            title = "Hmmm, that doesn't seem to be something I know how to do."
-            text = "Care to give me some feedback?"
-            actions = [
-              {
-                "name": "unknown_response",
-                "text": "Yes",
-                "type": "button",
-                "value": "yes"
-              },
-              {
-                "name": "unknown_response",
-                "text": "No",
-                "type": "button",
-                "value": "no"
-              }
-            ]
+          case button_value
+          when "report" then attachments_for_report
+          when "guidance" then attachments_for_guidance
+          else attachments_for_unknown
           end
+        end
 
+        ####################################################
+        # TODO: All of the below should live in yaml files #
+        ####################################################
+
+        def attachments_for_report
           [
             {
               "attachment_type": "default",
-              "title": title,
-              "text": text,
-              "actions": actions
+              "title":   ":writing_hand: You asked to create a report.",
+              "text":    "You'll need to speak to the HR team to make a "\
+                         "formal report. Can I pass your details onto them?",
+              "actions": [
+                {
+                  "name": "report_response",
+                  "text": "Yes",
+                  "type": "button",
+                  "value": "yes"
+                },
+                {
+                  "name": "report_response",
+                  "text": "No",
+                  "type": "button",
+                  "value": "no"
+                }
+              ]
+            }
+          ]
+        end
+
+        def attachments_for_guidance
+          [
+            {
+              "attachment_type": "default",
+              "title":   ":speaking_head_in_silhouette: You asked for some "\
+                         "advice.",
+              "text":    "I know about your HR policy and all the support "\
+                         "options available. Is any of that helpful?",
+              "actions": [
+                {
+                  "name": "guidance_response",
+                  "text": "Yes",
+                  "type": "select",
+                  "options": [
+                    {
+                      "text": "Our policies",
+                      "value": "policies"
+                    },
+                    {
+                      "text": "Support options",
+                      "value": "support"
+                    }
+                  ]
+                },
+                {
+                  "name": "guidance_response",
+                  "text": "No",
+                  "type": "button",
+                  "value": "no"
+                }
+              ]
+            }
+          ]
+        end
+
+        def attachments_for_unknown
+          [
+            {
+              "attachment_type": "default",
+              "title":   "Hmmm, that doesn't seem to be something I know how "\
+                         "to do.",
+              "text":    "Care to give me some feedback?",
+              "actions": [
+                {
+                  "name": "unknown_response",
+                  "text": "Yes",
+                  "type": "button",
+                  "value": "yes"
+                },
+                {
+                  "name": "unknown_response",
+                  "text": "No",
+                  "type": "button",
+                  "value": "no"
+                }
+              ]
             }
           ]
         end
